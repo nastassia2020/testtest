50069c8548325271f25a5dd1076a82afbc5457dd
nastassia2020 <wcganastacia@yandex.ru>
2024-01-22 11:35:00 +0300

    added functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Kettle from "./components/Kettle";
 import "./App.css";
+import Vitek from "./img/kettle.png";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <h1>Электрический чайник</h1>
+        <img src={Vitek} alt="Kettle" />
         <Kettle />
       </div>
     );
--- src/components/Kettle.jsx
+++ src/components/Kettle.jsx
@@ -24,23 +24,63 @@ export default class Kettle extends React.Component {
   }
 
   boilWater() {
-    setTimeout(() => {
-      if (this.state.isOn) {
-        this.setState({
-          isOn: false,
-        });
-        console.log("Чайник вскипел");
+    const interval = setInterval(() => {
+      if (this.state.isOn && this.state.temperature < 100) {
+        this.setState((prevState) => ({
+          temperature: prevState.temperature + 10,
+        }));
+      } else {
+        clearInterval(interval);
+        if (this.state.isOn) {
+          this.setState({
+            isOn: false,
+          });
+          console.log("Чайник вскипел");
+        }
       }
-    }, 10000);
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  handleWaterLevelChange(event) {
+    const waterLevel = parseFloat(event.target.value);
+    if (waterLevel >= 0 && waterLevel <= 1.0) {
+      this.setState({
+        waterLevel: waterLevel,
+      });
+    }
   }
 
   render() {
     return (
       <div>
-        <button onClick={() => this.togglePower()}>
+        <button
+          onClick={() => this.togglePower()}
+          disabled={this.state.waterLevel === 0}
+        >
           {this.state.isOn ? "Выключить" : "Включить"}
         </button>
-        {this.state.isOn && <p>Температура: {this.state.temperature}</p>}
+        <p>Количество воды: {this.state.waterLevel}</p>
+        <input
+          type="number"
+          step="0.1"
+          min="0"
+          max="1"
+          value={this.state.waterLevel}
+          onChange={(event) => this.handleWaterLevelChange(event)}
+        />
+        {this.state.waterLevel === 0 && <p>Чайник пуст! Добавьте воду.</p>}
+        <p>Время закипания: 10 секунд</p>
+        {!this.state.isOn &&
+          this.state.temperature !== 20 &&
+          this.state.temperature < 100 && <p>Чайник остановлен!</p>}
+        {this.state.temperature < 100 && (
+          <p>Температура: {this.state.temperature}</p>
+        )}
+        {this.state.temperature === 100 && <p>Чайник вскипел!</p>}
       </div>
     );
   }
